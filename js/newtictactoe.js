cf1f7fd97b82755d3e07cdc7c5524aedbaefe2e6
LPayyapilli <luke.payyapilli@gmail.com>
2015-05-14 11:47:44 -0400

    "All Done"

--- js/newtictactoe.js
+++ js/newtictactoe.js
@@ -4,16 +4,17 @@ $(document).ready(function() {
     var move = "#" + e.target.id;
         if(!$(move).html()) {
     turn();
-      $(move).html(player? "O" : "X");
-
+      $(move).html(player? $('#image2').html() : $('#image1').html());
+        $(move).show();
       board[e.target.id] = players[player];
       console.log(board);
+      turnCount +=1;
       checkForWin();
+
      }
    }
-});
+  });
   $("#reset").click(function() {
       reStartGame();
   });
-
 });
